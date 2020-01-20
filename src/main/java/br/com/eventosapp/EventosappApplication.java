429aca0a312231a062f65cadd9c29067773f5cda
karasurage <nicholas.mateus@gmail.com>
2020-01-20 17:49:59 -0300

    Testes para implementação

--- src/main/java/br/com/eventosapp/EventosappApplication.java
+++ src/main/java/br/com/eventosapp/EventosappApplication.java
@@ -2,9 +2,11 @@ package br.com.eventosapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.eventosapp.controllers")
 public class EventosappApplication {
 
 	public static void main(String[] args) {
